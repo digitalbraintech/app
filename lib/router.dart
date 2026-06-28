dfede68499ad1a3e7cfc56a6d7ef1370c684c1f1
Vladyslav Horbachov <0965142838gvs@gmail.com>
2026-06-28 19:09:04 +0200

    feat(app): experience-host route renders a guided journey full-screen, advancing in place

--- lib/router.dart
+++ lib/router.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import 'features/canvas/living_canvas_screen.dart';
+import 'features/experience/experience_host_screen.dart';
 import 'features/spike/globe_lottie_spike.dart';
 import 'shell/forui_app_shell.dart';
 
@@ -40,6 +41,19 @@ final digitalbrainRouter = GoRouter(
       path: '/canvas',
       name: 'canvas',
       builder: (context, state) => const LivingCanvasScreen(),
+    ),
+    GoRoute(
+      path: '/experience',
+      name: 'experience',
+      builder: (context, state) => const ExperienceHostScreen(),
+    ),
+    GoRoute(
+      path: '/experience/:pack/:experienceId',
+      name: 'experience-targeted',
+      builder: (context, state) => ExperienceHostScreen(
+        pack: state.pathParameters['pack'],
+        experienceId: state.pathParameters['experienceId'],
+      ),
     ),
     GoRoute(
       path: '/spike',
